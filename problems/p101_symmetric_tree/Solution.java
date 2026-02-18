f2c6ff0596d626c40172a2c327e97fe3c677de3e
clinton1719 <clintoncleetus17@gmail.com>
2026-02-18 21:28:17 +0530

    Symmetric Tree

--- problems/p101_symmetric_tree/Solution.java
+++ problems/p101_symmetric_tree/Solution.java
@@ -7,44 +7,50 @@ import problems.TreeNodeUtility;
 public class Solution {
   static void main() {
     Solution solution = new Solution();
-    TreeNode p = TreeNodeUtility.buildTree("1,2,2,3,4,4,3");
+    TreeNode p = TreeNodeUtility.buildTree("1,2,2,null,3,null,3");
     System.out.println(solution.isSymmetric(p));
   }
 
   public boolean isSymmetric(TreeNode root) {
     Stack<TreeNode> left = new Stack<>();
     Stack<TreeNode> right = new Stack<>();
-    if (root.left != null) {
-      left.add(root.left);
-    }
-    if (root.right != null) {
-      right.add(root.right);
-    }
+    left.add(root.left);
+    right.add(root.right);
 
     while (!left.isEmpty() && !right.isEmpty()) {
       int n = left.size();
+
       for (int i = 0; i < n; i++) {
         TreeNode leftNode = left.pop();
         TreeNode rightNode = right.pop();
 
+        if (leftNode == null && rightNode != null) return false;
+        if (leftNode != null && rightNode == null) return false;
+
+        if (leftNode == null && rightNode == null) continue;
+
         if (leftNode.val != rightNode.val) return false;
 
-        if (leftNode.left != null) {
-          left.add(leftNode.left);
-        }
-        if (leftNode.right != null) {
-          left.add(leftNode.right);
-        }
+        left.add(leftNode.left);
+        left.add(leftNode.right);
 
-        if (rightNode.right != null) {
-          left.add(rightNode.right);
-        }
-        if (rightNode.left != null) {
-          left.add(rightNode.left);
-        }
+        right.add(rightNode.right);
+        right.add(rightNode.left);
       }
     }
 
     return left.isEmpty() && right.isEmpty();
+  }
+
+  public boolean isSymmetric2(TreeNode root) {
+    return root==null || isSymmetricHelp(root.left, root.right);
+  }
+
+  private boolean isSymmetricHelp(TreeNode left, TreeNode right){
+    if(left==null || right==null)
+      return left==right;
+    if(left.val!=right.val)
+      return false;
+    return isSymmetricHelp(left.left, right.right) && isSymmetricHelp(left.right, right.left);
   }
 }
